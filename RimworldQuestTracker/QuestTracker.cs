b1ba8eb6d837502a7c26cc3cd6b63b1216f9e165
Elrcx <61328598+Elrcx@users.noreply.github.com>
2024-04-26 13:41:02 +0200

    Small refactor, moved on map code part into separate class.

--- RimworldQuestTracker/QuestTracker.cs
+++ RimworldQuestTracker/QuestTracker.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using UnityEngine;
 using Verse;
 
 namespace RimworldQuestTracker
@@ -22,80 +21,11 @@ namespace RimworldQuestTracker
             if (Current.Game != null && Current.Game.CurrentMap != null)
             {
                 // Register the MapComponent if it's not already registered.
-                if (Current.Game.CurrentMap.GetComponent<MapComponent_QuestTracker>() == null)
+                if (Current.Game.CurrentMap.GetComponent<QuestTrackerMap>() == null)
                 {
-                    Current.Game.CurrentMap.components.Add(new MapComponent_QuestTracker(Current.Game.CurrentMap));
+                    Current.Game.CurrentMap.components.Add(new QuestTrackerMap(Current.Game.CurrentMap));
                 }
             }
-        }
-    }
-
-    public class MapComponent_QuestTracker : MapComponent
-    {
-        private bool initialized = false;
-        private bool isDragging = false;
-        private Vector2 dragStartPos = Vector2.zero;
-        private Rect questTrackerRect = new Rect(UI.screenWidth - 200f, 20f, 180f, 30f);
-
-        public MapComponent_QuestTracker(Map map) : base(map)
-        {
-        }
-
-        public override void MapComponentUpdate()
-        {
-            base.MapComponentUpdate();
-
-            if (!initialized && Find.MainTabsRoot != null && Find.CurrentMap == map)
-            {
-                initialized = true;
-            }
-        }
-
-        public override void MapComponentOnGUI()
-        {
-            base.MapComponentOnGUI();
-
-            if (initialized)
-            {
-                HandleInput();
-                DrawQuestTracker();
-            }
-        }
-
-        private void HandleInput()
-        {
-            Event e = Event.current;
-
-            if (e.type == EventType.MouseDown && e.button == 0 && questTrackerRect.Contains(e.mousePosition))
-            {
-                isDragging = true;
-                dragStartPos = e.mousePosition - questTrackerRect.position;
-                e.Use();
-            }
-            else if (isDragging && e.type == EventType.MouseUp && e.button == 0)
-            {
-                isDragging = false;
-                e.Use();
-            }
-
-            if (isDragging && e.type == EventType.MouseDrag)
-            {
-                questTrackerRect.position = e.mousePosition - dragStartPos;
-                e.Use();
-            }
-        }
-
-        private void DrawQuestTracker()
-        {
-            Text.Anchor = TextAnchor.UpperRight;
-
-            Texture2D backgroundTexture = SolidColorMaterials.NewSolidColorTexture(new Color(0f, 0f, 0f, 0.5f));
-            GUI.DrawTexture(questTrackerRect, backgroundTexture);
-
-            GUI.color = Color.white;
-            Widgets.Label(questTrackerRect, "Quest Tracker");
-
-            Text.Anchor = TextAnchor.UpperLeft;
         }
     }
 }
---
+++ RimworldQuestTracker/QuestTrackerMap.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RimworldQuestTracker
+{
+    internal class QuestTrackerMap : MapComponent
+    {
+        private bool initialized = false;
+        private bool isDragging = false;
+        private Vector2 dragStartPos = Vector2.zero;
+        private Rect questTrackerRect = new Rect(UI.screenWidth - 200f, 20f, 180f, 30f);
+
+        public QuestTrackerMap(Map map) : base(map)
+        {
+
+        }
+
+        public override void MapComponentUpdate()
+        {
+            base.MapComponentUpdate();
+
+            if (!initialized && Find.MainTabsRoot != null && Find.CurrentMap == map)
+            {
+                initialized = true;
+            }
+        }
+
+        public override void MapComponentOnGUI()
+        {
+            base.MapComponentOnGUI();
+
+            if (initialized)
+            {
+                HandleInput();
+                DrawQuestTracker();
+            }
+        }
+
+        private void HandleInput()
+        {
+            Event e = Event.current;
+
+            if (e.type == EventType.MouseDown && e.button == 0 && questTrackerRect.Contains(e.mousePosition))
+            {
+                isDragging = true;
+                dragStartPos = e.mousePosition - questTrackerRect.position;
+                e.Use();
+            }
+            else if (isDragging && e.type == EventType.MouseUp && e.button == 0)
+            {
+                isDragging = false;
+                e.Use();
+            }
+
+            if (isDragging && e.type == EventType.MouseDrag)
+            {
+                questTrackerRect.position = e.mousePosition - dragStartPos;
+                e.Use();
+            }
+        }
+
+        private void DrawQuestTracker()
+        {
+            Text.Anchor = TextAnchor.UpperRight;
+
+            Texture2D backgroundTexture = SolidColorMaterials.NewSolidColorTexture(new Color(0f, 0f, 0f, 0.5f));
+            GUI.DrawTexture(questTrackerRect, backgroundTexture);
+
+            GUI.color = Color.white;
+            Widgets.Label(questTrackerRect, "Quest Tracker");
+            Widgets.Label(questTrackerRect, "Test");
+
+            Text.Anchor = TextAnchor.UpperLeft;
+        }
+    }
+}
