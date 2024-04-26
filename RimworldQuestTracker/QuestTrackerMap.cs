b5a29eca0ebe5eb7add5ca8d29c24a14f20e4ca4
Elrcx <61328598+Elrcx@users.noreply.github.com>
2024-04-26 21:48:51 +0200

    Preparing for quest list display.

--- RimworldQuestTracker/QuestTrackerMap.cs
+++ RimworldQuestTracker/QuestTrackerMap.cs
@@ -15,6 +15,8 @@ namespace RimworldQuestTracker
         private Vector2 dragStartPos = Vector2.zero;
         private Rect questTrackerRect = new Rect(UI.screenWidth - 200f, 20f, 180f, 30f);
 
+        int amount = UnityEngine.Random.Range(2, 6);  // for testing.
+
         public QuestTrackerMap(Map map) : base(map)
         {
 
@@ -66,16 +68,36 @@ namespace RimworldQuestTracker
 
         private void DrawQuestTracker()
         {
-            Text.Anchor = TextAnchor.UpperRight;
+            Text.Anchor = TextAnchor.UpperLeft;
 
-            Texture2D backgroundTexture = SolidColorMaterials.NewSolidColorTexture(new Color(0f, 0f, 0f, 0.5f));
-            GUI.DrawTexture(questTrackerRect, backgroundTexture);
+            // Define row sizes.
+            float rowHeight = 20f;
+            float rowIndentation = 20f;
 
-            GUI.color = Color.white;
-            Widgets.Label(questTrackerRect, "Quest Tracker");
-            Widgets.Label(questTrackerRect, "Test");
+            // Calculate vertical position.
+            float yOffset = questTrackerRect.y;
+
+            yOffset += rowHeight;
+
+            // Draw draggable handle.
+            Rect labelRect = new Rect(questTrackerRect.x, yOffset, questTrackerRect.width, rowHeight);
+            GUI.Label(labelRect, "▼ Tracked quests:");
+            yOffset += rowHeight;
+
+            // Draw quest information.
+            for (int i = 0; i < amount; i++)
+            {
+                Rect questRect = new Rect(questTrackerRect.x, yOffset, questTrackerRect.width, rowHeight);
+                GUI.Label(questRect, "► <b>Quest " + (i + 1) + "</b>");
+                yOffset += rowHeight;
+
+                Rect questDetailsRect = new Rect(questTrackerRect.x + rowIndentation, yOffset, questTrackerRect.width - rowIndentation, rowHeight);
+                GUI.Label(questDetailsRect, "<i>details</i>");
+                yOffset += rowHeight;
+            }
 
             Text.Anchor = TextAnchor.UpperLeft;
         }
+
     }
 }
